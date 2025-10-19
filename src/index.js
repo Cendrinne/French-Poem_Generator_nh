ae9665b2604cccdfee423556f321c3f7ef79868d
Cendrinne <nancy13lg@gmail.com>
2025-10-19 15:20:01 -0400

    js removed all consule logs

--- src/index.js
+++ src/index.js
@@ -21,10 +21,6 @@ function generatePoem(event) {
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="generating">⏳Generating a French poem about ${instructionsInput.value}</div`;
 
-  console.log("Generating poem...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
-
   axios.get(apiUrl).then(displayPoem);
 }
 
