0da5c6d3ee73c10213ed04bb19079c3402c496aa
Cendrinne <nancy13lg@gmail.com>
2025-10-19 15:02:57 -0400

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "73f90ofbe4423241037fca0b58a62fdt";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is in HTML format, generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is in HTML format, generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Do not include markdown in your response. Sign the poem with 'SheCodes AI' on it own line, inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating a French poem about ${instructionsInput.value}</div`;
 
   console.log("Generating poem...");
   console.log(`Prompt: ${prompt}`);
